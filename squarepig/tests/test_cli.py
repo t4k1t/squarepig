581abeea40aece291e55c5b1a82826dc77af8b6b
tablet-mode <tablet-mode@monochromatic.cc>
2014-08-18 20:25:29 +0200

    Add test env

--- squarepig/tests/test_cli.py
+++ squarepig/tests/test_cli.py
@@ -11,7 +11,7 @@ class TestCLI:
     """Test CLI."""
 
     def test_cli(self, capsys, monkeypatch):
-        """Test CLI."""
+        """Test help text output."""
         monkeypatch.setattr('sys.argv', ['squarepig.py', '-h'])
         with pytest.raises(SystemExit):
             main()
@@ -19,14 +19,18 @@ class TestCLI:
         assert "usage:" in out.lower()
 
     def test_version(self, capsys, monkeypatch):
-        """Test CLI."""
+        """Test version string output."""
         monkeypatch.setattr('sys.argv', ['squarepig.py', '-V'])
         with pytest.raises(SystemExit):
             main()
         out, err = capsys.readouterr()
-        assert "Squarepig {version}".format(version=__version__) in out
+        # in python-3.3 argparse writes the version string to stderr but in
+        # python-3.4 it writes to stdout so both is acceptable
+        assert "Squarepig {version}".format(version=__version__) in (
+            out or err)
 
     def test_invalid_playlist(self, capsys, monkeypatch):
+        """Test invalid playlist argument."""
         monkeypatch.setattr('sys.argv', ['squarepig.py', '-p',
                             'totally_invalid_playlist_path', '-d',
                             'invalid_destination_path'])
@@ -36,6 +40,7 @@ class TestCLI:
         assert "unable to find" in err
 
     def test_missing_playlist(self, capsys, monkeypatch):
+        """Test missing playlist argument."""
         monkeypatch.setattr('sys.argv', ['squarepig.py', '-d',
                             'totally_invalid_destination_path'])
         with pytest.raises(SystemExit):
